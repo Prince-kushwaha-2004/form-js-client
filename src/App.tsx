7883ef9a56bfa5fcdcbef78b752e356a69941146
prince <prince121kk@gmail.com>
2025-03-04 22:04:29 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Form from "./hooks/Form"
+
+function App() {
+  // let data = {
+  //   onSubmit: (d) => {
+  //     console.log(d)
+  //   },
+  //   className: "",
+  //   form: [
+  //     {
+  //       type: "text",
+  //       requried: true,
+  //       label: "label",
+  //       name: "name",
+  //     }
+  //   ]
+  // }
+  return (
+    <>
+      <Form />
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/Form.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+function Form(): React.ReactElement {
+    return (
+        <main className="w-screen h-screen p-8">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <h1 className="text-2xl font-bold text-center mb-4">Dynamic Form</h1>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                </div>
+                <div className="mb-6">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
+                        State
+                    </label>
+                    <div className="relative">
+                        <select className="block appearance-none w-full  border text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                            <option>New Mexico</option>
+                            <option>Missouri</option>
+                            <option>Texas</option>
+                        </select>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Sign In
+                    </button>
+                </div>
+            </form>
+        </main>
+    )
+}
+
+export default Form
